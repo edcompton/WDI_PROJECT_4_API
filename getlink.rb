1a125acc63fcb6de84cad9f5b5e0babc804372da
HenryDavies <henrywdavies@gmail.com>
2017-02-16 18:00:09 +0000

    adding raw htmls for income statements and getlink.rb file

---
+++ getlink.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+ticker = "GOOG"
+link_array = []
+
+xml = "https://www.sec.gov/cgi-bin/browse-edgar?CIK=#{ticker}&Find=Search&owner=exclude&action=getcompany&output=atom&type=10-k"
+doc = Nokogiri::XML(open(xml))
+
+doc.css('filing-href').each do |x|
+  link_array << (x.text.split('/')[0..-2] << "R2.htm").join('/')
+end
+
+puts link_array
