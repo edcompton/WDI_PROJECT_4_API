d6feb2079ba54f32c73155ab3929673b89173dce
edcompton <eahcompton@gmail.com>
2017-02-18 16:51:54 +0000

    scraper working for all statements and files saved

--- ed_compton/scraping/scrape.rb
+++ ed_compton/scraping/scrape.rb
@@ -2,6 +2,12 @@
 # Query each of the company's 10-k listed statements
 # and access each of the consolidated financial statements
 
+
+# Create a folder directory for that ticker item
+# Create a folder directory for that year
+# Create 4 files within each for each of meta data, income statement, cash flow
+# Append content gathered from the scraper into the created files
+
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
@@ -10,7 +16,7 @@ require 'fileutils'
 class SheetSelector
 
   SEC_URL   = "https://www.sec.gov/cgi-bin/browse-edgar"
-  R_NUMBERS = (2..4)
+  R_NUMBERS = (1..9)
 
   attr_reader :ticker, :doc
   attr_accessor :sheet_urls, :statements
@@ -18,10 +24,6 @@ class SheetSelector
   def initialize ticker
     @ticker               = ticker
     @sheet_urls           = []
-    @balance_sheets       = []
-    @income_statements    = []
-    @cash_flow_statements = []
-    @R1_statements        = []
     run
   end
 
@@ -71,17 +73,17 @@ class SheetSelector
     sheet_urls.each do |sheet_url|
       begin
         r_statement = Nokogiri::XML(open(sheet_url))
-        statement_title = r_statement.css("th[class=tl]").to_s
-        @statement_year = r_statement.xpath("//body//tr//th[@class='th']")[0].text.to_s[-4..-1]
-        create_folder_year
-        @R1_statements << r_statement if statement_title.include? "Entity Information"
+        statement_title = r_statement.css("th[class=tl]").text.to_s
+        if statement_title.include? "Entity Information"
+          @statement_year = r_statement.xpath("//body//tr//th[@class='th']")[3].text.to_s[-4..-1]
+          create_folder_year
+          create_file(statement_title, r_statement)
+        end
         sleep 2
       rescue OpenURI::HTTPError
         next
       end
     end
-    # puts @balance_sheets
-    puts @statement_year
   end
 
   def find_balance_sheets
@@ -89,10 +91,9 @@ class SheetSelector
       begin
         r_statement = Nokogiri::XML(open(sheet_url))
         statement_title = r_statement.css("th[class=tl]").text.to_s
-        @statement_year = r_statement.css("th[class=th]")[1].text.to_s[-4..-1]
-        create_folder_year
-        # @balance_sheets << r_statement
         if statement_title.include? "BALANCE"
+          @statement_year = r_statement.css("th[class=th]")[0].text.to_s[-4..-1]
+          create_folder_year
           create_file(statement_title, r_statement)
         end
         sleep 2
@@ -100,8 +101,6 @@ class SheetSelector
         next
       end
     end
-    # puts @balance_sheets
-    puts @statement_year
   end
 
   def find_income_statements
@@ -109,43 +108,38 @@ class SheetSelector
       begin
         r_statement = Nokogiri::XML(open(sheet_url))
         statement_title = r_statement.css("th[class=tl]").text.to_s
-        @statement_year = r_statement.css("th[class=th]")[1].text.to_s[-4..-1]
-        create_folder_year
-
-        # if statement_title.!include? "Parenthetical" and statement_title.!include? "COMPREHENSIVE"
+        # if statement_title.exclude? "Parenthetical"
         if statement_title.include? "INCOME" or statement_title.include? "EARNINGS" or statement_title.include? "OPERATIONS"
+          @statement_year = r_statement.css("th[class=th]")[1].text.to_s[-4..-1]
+          create_folder_year
           create_file(statement_title, r_statement)
         end
-        # end
-        # @income_statements << r_statement
         # end
         sleep 2
       rescue OpenURI::HTTPError
         next
       end
     end
-    # puts @income_statements
-    puts @statement_year
   end
 
   def find_cash_flow
     sheet_urls.each do |sheet_url|
       begin
-        r_statement = Nokogiri::XML(open(sheet_url)).to_s
-        statement_title = r_statement.css("th[class=tl]").text.to_s
-        @statement_year = r_statement.css("th[class=th]")[1].text.to_s[-4..-1]
-        create_folder_year
-        # @cash_flow_statements << r_statement
-        if statement_title.include? "CASH"
-          create_file(statement_title, r_statement)
+        r_statement = Nokogiri::XML(open(sheet_url))
+        if r_statement != nil
+          statement_title = r_statement.css("th[class=tl]").text.to_s
+          if statement_title.include? "CASH"
+            @statement_year = r_statement.xpath("//body//tr//th[@class='th']")[1].text.to_s[-4..-1]
+            puts @statement_year
+            create_folder_year
+            create_file(statement_title, r_statement)
+          end
         end
         sleep 2
       rescue OpenURI::HTTPError
         next
       end
     end
-    # puts @cash_flow_statements
-    puts @statement_year
   end
 
   def create_folder_year
@@ -159,42 +153,7 @@ class SheetSelector
     blank_file = File.new("ed_compton/scraping/scraped_files/#{@ticker}/#{@statement_year}/#{statement_title}.html", "w+")
     blank_file.puts (r_statement)
   end
-
-
-  # Create a folder directory for that ticker item
-  # Create a folder directory for that year
-  # Create 4 files within each for each of meta data, income statement, cash flow
-  # Append content gathered from the scraper into the created files
-
 end
 
 
 SheetSelector.new "GOOG"
-
-
-
-# blank_file = File.new("apple.xml", "w+")
-#
-# apple_balance_sheet = ("/Users/jackfuller/development/WDI_PROJECT_4_API/raw_htmls/balance_sheets/apple_BS.xml")
-#
-# @doc = File.open(apple_balance_sheet) { |f| Nokogiri::XML(f) }
-
-# x = @doc.xpath("//cashandcashequivalent")
-# x = @doc.css('td')
-# val = @doc.xpath("//a and text() = Cash And Cash Equivalents")
-# val = @doc.xpath("//a//@onclick")
-# val.each do |onclick|
-#   if (onclick.value.include? "Cash")
-#     p onclick
-#     p onclick.xpath("/td")
-#   end
-#
-# end
-#
-# def find_cash_cash_equivalents
-#
-# end
-
-
-# File.open(blank_file, "w") { |file| file.write(onclick)}
-# xml = open(url)
