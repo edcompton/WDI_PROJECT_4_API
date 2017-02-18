924f1d3f0809f97ae592a14a4cd3f4c9c8205468
HenryDavies <henrywdavies@gmail.com>
2017-02-18 19:24:18 +0000

    minor change to historical prices yahoo file - adding date functionality

---
+++ henry_davies/historical_prices_yahoo.rb
@@ -0,0 +1,20 @@
+require 'httparty'
+require 'date'
+
+stocks = ['AAPL','KO','PG','GOOG']
+
+stocks.each do |ticker|
+  url = "http://query.yahooapis.com/v1/public/yql?q=
+  select * from   yahoo.finance.historicaldata
+           where  symbol    = '#{ticker}'
+           and    startDate = '2016-01-01'
+           and    endDate   = '#{Date.today.strftime("%Y-%m-%d")}'
+  &format=json
+  &diagnostics=true
+  &env=store://datatables.org/alltableswithkeys
+  &callback="
+
+  response = HTTParty.get(url)
+  priceHistory = p response['query']['results']['quote']
+  puts priceHistory
+end
