10d2bda1140e0a1cf889a8c89a1a2c4402cab57c
jackhkmatthews <jackhkmatthews@gmail.com>
2017-02-21 11:11:46 +0000

    start on directory scrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220153442) do
+ActiveRecord::Schema.define(version: 20170221094737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 20170220153442) do
     t.integer  "company_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "year"
     t.index ["company_id"], name: "index_filings_on_company_id", using: :btree
   end
 
