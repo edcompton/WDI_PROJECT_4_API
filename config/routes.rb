a6a533511ede066a6195d5a8b6c352c986b237a7
hachtman <jackfllr1@googlemail.com>
2017-02-21 19:58:50 +0000

    added user show route and usercontroller for front end logged in checking

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'companies', to: "companies#index"
-
-  get 'companies/:id', to: "companies#show"
-
+  get 'users/:id', to: "users#show"
   # setting routes with form shown below
   # VERB(URL, to: CONTROLLER#METHOD)
   post 'register', to: "authentications#register"
