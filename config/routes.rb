c349a7b2dc39c5e9eedcfb7c308bcfddac8a01a8
edcompton <eahcompton@gmail.com>
2017-02-22 10:03:37 +0000

    bull

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'companies', to: "companies#index"
 
+  get 'users/:id', to: "users#show"
+
   get 'companies/:id', to: "companies#show"
 
   # setting routes with form shown below
